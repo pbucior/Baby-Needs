e72582c73019c033832a7919b0eea21a67627898
Paweł Bucior <pawel@bucior.eu>
2020-03-02 22:20:16 +0100

    Constants util class

---
+++ app/src/main/java/eu/bucior/babyneeds/util/Constants.java
@@ -0,0 +1,14 @@
+package eu.bucior.babyneeds.util;
+
+public class Constants {
+    public static final int DB_VERSION = 1;
+    public static final String DB_NAME = "babyList";
+    public static final String TABLE_NAME = "baby_tbl";
+
+    public static final String KEY_ID = "id";
+    public static final String KEY_ITEM = "baby_item";
+    public static final String KEY_QUANTITY = "quantity_number";
+    public static final String KEY_COLOR = "color";
+    public static final String KEY_SIZE = "size";
+    public static final String KEY_DATE_ADDED = "date_added";
+}

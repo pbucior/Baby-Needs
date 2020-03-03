60bf113b11be3c69b15b5eca6e1114b3f3e3c332
Paweł Bucior <pawel@bucior.eu>
2020-03-03 23:37:04 +0100

    CRUD Operations

--- app/src/main/java/eu/bucior/babyneeds/data/DatabaseHandler.java
+++ app/src/main/java/eu/bucior/babyneeds/data/DatabaseHandler.java
@@ -1,11 +1,19 @@
 package eu.bucior.babyneeds.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import eu.bucior.babyneeds.model.Item;
 import eu.bucior.babyneeds.util.Constants;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
@@ -35,5 +43,116 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + Constants.TABLE_NAME);
         onCreate(db);
+    }
+
+    public void addItem(Item item) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Constants.KEY_ITEM, item.getItemName());
+        values.put(Constants.KEY_COLOR, item.getItemColor());
+        values.put(Constants.KEY_QUANTITY, item.getItemQuantity());
+        values.put(Constants.KEY_SIZE, item.getItemSize());
+        values.put(Constants.KEY_DATE_ADDED, java.lang.System.currentTimeMillis());
+
+        db.insert(Constants.TABLE_NAME, null, values);
+    }
+
+    public Item getItem(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(Constants.TABLE_NAME,
+                new String[]{Constants.KEY_ID,
+                        Constants.KEY_ITEM,
+                        Constants.KEY_QUANTITY,
+                        Constants.KEY_SIZE,
+                        Constants.KEY_DATE_ADDED},
+                Constants.KEY_ID + "=?",
+                new String[]{String.valueOf(id)}, null, null, null, null);
+
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        Item item = new Item();
+        if (cursor != null) {
+            item.setId(cursor.getInt(cursor.getColumnIndex(Constants.KEY_ID)));
+            item.setItemName(cursor.getString(cursor.getColumnIndex(Constants.KEY_ITEM)));
+            item.setItemQuantity(cursor.getInt(cursor.getColumnIndex(Constants.KEY_QUANTITY)));
+            item.setItemSize(cursor.getInt(cursor.getColumnIndex(Constants.KEY_SIZE)));
+
+            DateFormat dateFormat = DateFormat.getDateInstance();
+            String formattedDate = dateFormat.format(new Date(cursor.getLong(cursor.getColumnIndex(Constants.KEY_DATE_ADDED)))
+                    .getTime());
+            item.setDateItemAdded(formattedDate);
+
+        }
+
+        return item;
+    }
+
+    public List<Item> getAllItems() {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        List<Item> itemList = new ArrayList<>();
+
+        Cursor cursor = db.query(Constants.TABLE_NAME,
+                new String[]{Constants.KEY_ID,
+                        Constants.KEY_ITEM,
+                        Constants.KEY_QUANTITY,
+                        Constants.KEY_SIZE,
+                        Constants.KEY_DATE_ADDED},
+                null, null, null, null,
+                Constants.KEY_DATE_ADDED + " DESC");
+
+        if (cursor.moveToFirst()) {
+            do {
+                Item item = new Item();
+                item.setId(cursor.getInt(cursor.getColumnIndex(Constants.KEY_ID)));
+                item.setItemName(cursor.getString(cursor.getColumnIndex(Constants.KEY_ITEM)));
+                item.setItemQuantity(cursor.getInt(cursor.getColumnIndex(Constants.KEY_QUANTITY)));
+                item.setItemSize(cursor.getInt(cursor.getColumnIndex(Constants.KEY_SIZE)));
+
+                DateFormat dateFormat = DateFormat.getDateInstance();
+                String formattedDate = dateFormat.format(new Date(cursor.getLong(cursor.getColumnIndex(Constants.KEY_DATE_ADDED)))
+                        .getTime());
+                item.setDateItemAdded(formattedDate);
+
+                itemList.add(item);
+            } while (cursor.moveToNext());
+        }
+
+        return itemList;
+    }
+
+    public int updateItem(Item item) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Constants.KEY_ITEM, item.getItemName());
+        values.put(Constants.KEY_COLOR, item.getItemColor());
+        values.put(Constants.KEY_QUANTITY, item.getItemQuantity());
+        values.put(Constants.KEY_SIZE, item.getItemSize());
+        values.put(Constants.KEY_DATE_ADDED, java.lang.System.currentTimeMillis());
+
+        return db.update(Constants.TABLE_NAME, values, Constants.KEY_ID + "=?",
+                new String[]{String.valueOf(item.getId())});
+    }
+
+    public void deleteItem(int id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.delete(Constants.TABLE_NAME, Constants.KEY_ID + "=?",
+                new String[]{String.valueOf(id)});
+
+        db.close();
+    }
+
+    public int getItemsCount() {
+        String countQuery = "SELECT * FROM " + Constants.TABLE_NAME;
+        SQLiteDatabase db = getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(countQuery, null);
+
+        return cursor.getCount();
     }
 }
